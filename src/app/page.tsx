04220bc6227eb8de12adb1c1180df529a3d6e0d3
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2025-09-02 20:45:00 +0100

    Configure CSS modules
    - Use CSS rules that work with Chakra and modules
    - Test color mode button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { ColorModeButton } from "@/components/ui/color-mode";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <ColorModeButton />
       <main className={styles.main}>
         <Image
           className={styles.logo}
