6dc19cbead6aab4b8fa3bfb105736e76f8695db0
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2025-09-02 21:43:38 +0100

    Fix Tanstack Query error

---
+++ src/app/query-providers.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import type * as React from "react";
+import { getQueryClient } from "@/lib/query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export default function QueryProviders({ children }: { children: React.ReactNode }) {
+  const queryClient = getQueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
+}
