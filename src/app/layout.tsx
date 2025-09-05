6dbb8b67a08d48dd83eed0fde92e21f700d6e42b
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2025-09-05 17:29:58 +0100

    fix(i18n): Configure next-intl to use cookies
    When building, an error occurred because the root layout's children were not wrapped in the Chakra UI provider. When fixing that, I decided I'd rather have i18n be cookie based, rather than route based.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,46 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { getMessages, getLocale, getTranslations } from "next-intl/server";
 import "./globals.css";
+import {NextIntlClientProvider} from 'next-intl';
+import {Provider} from '@/components/ui/provider'; 
+import QueryProviders from './query-providers';
 
-export default function RootLayout({
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslations("Meta");
+  return {
+    title: t("title"),
+    description: t("description"),
+  };
+}
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return children;
+  const locale = await getLocale();
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <NextIntlClientProvider messages={messages} locale={locale} timeZone="UTC">
+          <QueryProviders>
+            <Provider>{children}</Provider>
+          </QueryProviders>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,35 @@
 "use client";
 
 import { Button } from "@chakra-ui/react";
+import { useTranslations } from "next-intl";
+
 import { ColorModeButton } from "@/components/ui/color-mode";
 import { useUIStore } from "@/lib/store";
 import styles from "./page.module.css";
 
 export default function Home() {
   const { mode, toggleMode } = useUIStore();
+  const t = useTranslations("Home");
 
   return (
     <div className={styles.page}>
       <header className={styles.header}>
-        <h1>SpacePlan</h1>
+        <h1>{t("title")}</h1>
         <ColorModeButton />
       </header>
       <main className={styles.main}>
         <p>
-          <span>We are currently in <code>{mode}</code> mode.</span>
-          <Button onClick={toggleMode}>Toggle Dimension Mode!</Button>
+          <span>
+            {t.rich("currentMode", { 
+              code: (chunks) => <code>{chunks}</code>,
+              mode
+            })}
+          </span>
+          <Button onClick={toggleMode}>{t("toggleButton")}</Button>
         </p>
       </main>
       <footer className={styles.footer}>
-        <p>Made with love by theAvocaodoCoder</p>
+        <p>{t("footer")}</p>
       </footer>
     </div>
   );
