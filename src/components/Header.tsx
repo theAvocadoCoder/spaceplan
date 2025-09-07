e015eeaadfc9c226c409096bd3b5486fa0a5f6e9
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2025-09-07 21:50:07 +0100

    feat(ui): Create shared layout components with responsive design

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useTranslations } from "next-intl";
+
+import { ColorModeButton } from "@/components/ui/color-mode";
+import styles from "./components.module.css";
+import Link from "next/link";
+
+export default function Header() {
+  const t = useTranslations("Home");
+  const pathname = usePathname();
+  const hideHeader = pathname.startsWith("/editor") || pathname.startsWith("/auth");
+
+  if (hideHeader) return null;
+
+  return (
+    <header className={styles.header}>
+      <Link href="/">{t("title")}</Link>
+      <ColorModeButton />
+    </header>
+  );
+}
